b1fe64d2fbeb914c9c8f95cdcb370c02bbf24cde
Colin Campbell-McPherson <colin@logaan.net>
2009-02-23 22:29:26 +0900

    added json api, group name validations and fixed child group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,6 @@ class GroupsController < ApplicationController
   make_resourceful do
     actions :all
     belongs_to :group
+    publish :json, :attributes => [:parent_id, :name]
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,11 @@
 class Group < ActiveRecord::Base
+  # Validations
+  validates_presence_of   :name
+  validates_uniqueness_of :name
+
+  # Plugins
   acts_as_tree
+
+  # Virtual Attributes
   alias_method :groups, :children
 end
