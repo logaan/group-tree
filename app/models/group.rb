0797b98f663c5ee64b4d987b49c0ac93bdfdaf0e
Colin Campbell-McPherson <colin@logaan.net>
2009-02-23 22:05:09 +0900

    did basic plugin setup

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ActiveRecord::Base
+  acts_as_tree
+  alias_method :groups, :children
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :groups
+  map.root :controller => "groups"
+
+  map.resources :groups, :has_many => :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
 
