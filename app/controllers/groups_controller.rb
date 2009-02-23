ed899a31807e5b00f246fd8b6e2de58eaf255ed3
Colin Campbell-McPherson <colin@logaan.net>
2009-02-23 22:39:50 +0900

    only display root groups at /groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,9 @@ class GroupsController < ApplicationController
     belongs_to :group
     publish :json, :attributes => [:parent_id, :name]
   end
+
+  def current_objects
+    @current_objects ||= parent_model ?
+      current_model.find(:all) : current_model.roots
+  end
 end
